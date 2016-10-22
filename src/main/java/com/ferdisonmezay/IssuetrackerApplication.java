66abf13a93be341997d62e175981b93f816530eb
Ferdi Sonmezay <ferdi.maillist@gmail.com>
2016-10-22 15:29:58 +0300

    initial project, downloaded from spring.io

---
+++ src/main/java/com/ferdisonmezay/IssuetrackerApplication.java
@@ -0,0 +1,12 @@
+package com.ferdisonmezay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuetrackerApplication.class, args);
+	}
+}
